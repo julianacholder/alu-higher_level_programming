df2ac3604fe1e083a62bffa9fbc744d225560d8b
julianacholder <j.holder@alustudent.com>
2023-07-27 19:58:30 -0700

    0-rectangle.py

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-  An empty rectangle class
-  """
+An empty rectangle class
+"""
 
 class Rectangle:
-    """  Creates the class rectangle """
+    """Creates the class rectangle"""
     pass
