bc17f4bbb19757890342f2573d7c71b20954e9d4
julianacholder <j.holder@alustudent.com>
2023-08-04 07:34:43 -0700

    0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a request and displays the size of the response body
-curl -sI "$1" | sed -En "s/Content-Length: (.*)/\1/p"
+# Script that shows the Content-Length from a HTTP request
+curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
