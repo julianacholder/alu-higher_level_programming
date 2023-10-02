247d1cde53083f3fde083ff2edafc3665f1b6928
root <j.holder@alustudent.com>
2023-10-02 10:34:43 +0000

    add

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -2,21 +2,8 @@
 const request = require('request');
 const fs = require('fs');
 
-const url = process.argv[2];
-const file_path = process.argv[3];
-
-if (typeof url === 'string') {
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      fs.writeFile(file_path, body, 'utf8', function (err) {
-        if (err) {
-          console.error('Error writing to file:', err);
-          throw err; // You can throw the error if needed
-        }
-        console.log(`Webpage content successfully downloaded and saved to ${file_path}`);
-      });
-    } else {
-      console.error('Error:', error);
-    }
-  });
-}
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    fs.writeFileSync(process.argv[3], body);
+  }
+});
