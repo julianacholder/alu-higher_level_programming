a802f138452338d90a1e2a902b43c1d904b15b3f
root <j.holder@alustudent.com>
2023-10-02 10:12:40 +0000

    task 5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -2,16 +2,21 @@
 const request = require('request');
 const fs = require('fs');
 
-request(process.argv[2], function (err, response, body) {
-  if (err == null) {
-    fs.writeFileSync(process.argv[3], body);
-  }
-});#!/usr/bin/node
-const request = require('request');
-const fs = require('fs');
+const url = process.argv[2];
+const outputPath = process.argv[3];
 
-request(process.argv[2], function (err, response, body) {
-  if (err == null) {
-    fs.writeFileSync(process.argv[3], body);
+request(url, function (err, response, body) {
+  if (err) {
+    console.error('Error:', err);
+    process.exit(1); // Exit with an error code
   }
+
+  if (response.statusCode !== 200) {
+    console.error('Error: HTTP Status Code', response.statusCode);
+    process.exit(1); // Exit with an error code
+  }
+
+  // Write the webpage content to the specified file with UTF-8 encoding
+  fs.writeFileSync(outputPath, body, 'utf-8');
+  console.log(`Webpage content successfully downloaded and saved to ${outputPath}`);
 });
